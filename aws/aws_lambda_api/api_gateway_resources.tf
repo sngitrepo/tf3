eb0cab803f74cda9a5a3fdeb37a3e949bb0a3314
Srividhya0510 <88324748+Srividhya0510@users.noreply.github.com>
2022-06-29 11:53:20 +0530

    Update api_gateway_resources.tf

--- aws/aws_lambda_api/api_gateway_resources.tf
+++ aws/aws_lambda_api/api_gateway_resources.tf
@@ -24,12 +24,12 @@ resource "aws_api_gateway_resource" "proxy_other" {
   path_part   = "{proxy+}"
 }
 
-resource "aws_api_gateway_method" "proxy_other" {
+/*resource "aws_api_gateway_method" "proxy_other" {
   rest_api_id   = "${aws_api_gateway_rest_api.this.id}"
   resource_id   = "${aws_api_gateway_resource.proxy_other.id}"
   http_method   = "ANY"
   authorization = "NONE"
-}
+}*/
 
 resource "aws_api_gateway_integration" "proxy_other" {
   rest_api_id             = "${aws_api_gateway_rest_api.this.id}"
